a88106f015775397488c9e3f0735ea183bd97392
Billy Mitchell <wmitch25@uncc.edu>
2017-03-30 18:40:40 +0000

    Localy saved

--- uscan/config/routes.rb
+++ uscan/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'contact/index' , to: "contact#contact", as: "contact"
   get 'search/search' , to: "search#search", as: "search"
-  
+  get 'welcome/index' , to: "weclome#index", as: "back"
   resources :searchs
   resources :contacts
   
