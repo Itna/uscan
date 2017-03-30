9555b510613a6ade8a773f3365de91e9ed47c036
Jonathan Stanwic <Jstanwi1@gmail.com>
2017-03-30 17:38:21 +0000

    fixed buttons

--- uscan/config/routes.rb
+++ uscan/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'contact/index'
   get 'welcome/index'
-  get 'contact/index' , to: "contacts#index", as: "contact"
-  get 'search/search' , to: "searchs#search", as: "search"
+  get 'contact/index' , to: "contact#contact", as: "contact"
+  get 'search/search' , to: "search#search", as: "search"
   
   resources :searchs
   resources :contacts
