e965a1fe58cdebf8834c0830fbdd920826448349
Ryan Leigh <Ryan Leigh>
2017-04-06 18:01:06 +0000

    created form controller

--- uscan/config/routes.rb
+++ uscan/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'contact/index'
   get 'welcome/index'
-  get 'contact/index' , to: "contact#contact", as: "contact"
   get 'search/search' , to: "search#search", as: "search"
   get 'welcome/index' , to: "weclome#index", as: "back"
+  
   resources :searchs
   resources :contacts
+  resources :form
   
   root 'welcome#index'
 end
